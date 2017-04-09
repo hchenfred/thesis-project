5cb6598cac9d2293b8850cc1844ce574dc633c71
Huan Chen <hchen.fred@gmail.com>
2017-04-09 15:44:47 -0700

    (feat) able to get Facebook user name, email and photo

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactNative from 'react-native';
 
 const {
@@ -6,17 +6,50 @@ const {
 } = ReactNative;
 
 const FBSDK = require('react-native-fbsdk');
+
 const {
+  GraphRequest,
   LoginButton,
   AccessToken,
+  GraphRequestManager,
 } = FBSDK;
 
-var Login = React.createClass({
-  render: function() {
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      pic: '',
+    };
+    this.responseInfoCallback = this.responseInfoCallback.bind(this);
+  }
+
+
+  componentWillMount() {
+    // Create a graph request asking for user information with a callback to handle the response.
+    const infoRequest = new GraphRequest(
+      '/me?fields=name,picture,email',
+      null,
+      this.responseInfoCallback,
+    );
+    // Start the graph request.
+    new GraphRequestManager().addRequest(infoRequest).start();
+  }
+
+  responseInfoCallback(error, result) {
+    if (error) {
+      alert('Error fetching data: ' + error.toString());
+    } else {
+      console.log('hihihi=======>', result);
+      this.setState({ name: result.name, pic: result.picture.data.url });
+    }
+  }
+
+  render() {
     return (
       <View>
         <LoginButton
-          publishPermissions={["publish_actions"]}
+          publishPermissions={['publish_actions']}
           onLoginFinished={
             (error, result) => {
               if (error) {
@@ -24,20 +57,21 @@ var Login = React.createClass({
               } else if (result.isCancelled) {
                 alert("login is cancelled.");
               } else {
-                console.log('result is ', result);
+                console.log('result is ' + result);
                 AccessToken.getCurrentAccessToken().then(
                   (data) => {
+                    console.log('hi, data is ', data);
                     {/*alert('hello there', data);*/}
                     {/*alert('hello', data.accessToken.toString());*/}
-                  }
-                )
+                  },
+                );
               }
             }
           }
-          onLogoutFinished={() => alert("logout.")}/>
+          onLogoutFinished={() => alert("logout.")} />
       </View>
     );
   }
-});
+}
 
 export default Login;
