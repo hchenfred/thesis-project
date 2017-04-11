4f71e93cee7e94d439852a87528de2fc47665896
Huan Chen <hchen.fred@gmail.com>
2017-04-11 14:08:18 -0700

    (feat) add css style to AddFriends page

--- app/screens/AddFriends.js
+++ app/screens/AddFriends.js
@@ -4,6 +4,12 @@ import { View, ListView, StyleSheet, Text, TextInput, TouchableOpacity } from 'r
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#2ecc71',
+  },
+  formContainer: {
+    marginTop: 10,
+    padding: 20,
+    flexGrow: 1,
   },
   place: {
     height: 40,
@@ -28,7 +34,7 @@ class AddFriends extends React.Component {
   constructor(props) {
     super(props);
     // this.friendList = ['fred', 'hello', 'steve'];
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     this.state = {
       dataSource: ds.cloneWithRows([]),
       friendList: [],
@@ -39,39 +45,40 @@ class AddFriends extends React.Component {
   }
 
   onPressAddButton() {
-    console.log(this.state.friendName);
-    let temp = this.state.friendList.slice();
-    temp.push(this.state.friendName);
+    const temp = this.state.friendList.slice();
+    temp.push({ name: this.state.friendName, email: this.state.friendEmail });
     this.setState({ friendList: temp });
-    console.log(temp);
     this.setState({
-       dataSource: this.state.dataSource.cloneWithRows(temp),
+      dataSource: this.state.dataSource.cloneWithRows(temp),
     });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>'halal guys for Steve!!!!============='</Text>
-        <TextInput
-          onChangeText={(name) => this.setState({ friendName: name })}
-          style={styles.place}
-          placeholder="friend's name"
-        />
-        <TextInput
-          onChangeText={(email) => this.setState({ friendEmail: email })}
-          style={styles.place}
-          keyboardType="email-address"
-          autoCorrect={false}
-          placeholder="friend's email"
-        />
-        <TouchableOpacity onPress={this.onPressAddButton} style={styles.buttonContainer}>
-          <Text style={styles.buttonText}>Add to Invite List</Text>
-        </TouchableOpacity>
-        <View style={{flex: 1}}>
-          <ListView style={{ height: 600, width: 600 }}
+        <View style={styles.formContainer}>
+          <TextInput
+            onChangeText={name => this.setState({ friendName: name })}
+            style={styles.place}
+            placeholder="friend's name"
+          />
+          <TextInput
+            onChangeText={email => this.setState({ friendEmail: email })}
+            style={styles.place}
+            keyboardType="email-address"
+            autoCorrect={false}
+            placeholder="friend's email"
+          />
+          <TouchableOpacity onPress={this.onPressAddButton} style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>Add to Invite List</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{ flexGrow: 4, marginTop: 0 }}>
+          <ListView
+            enableEmptySections={true}
+            style={{ height: 600, width: 600 }}
             dataSource={this.state.dataSource}
-            renderRow={(rowData) => <Text>{rowData}</Text>}
+            renderRow={rowData => <Text>{rowData.name}</Text>}
           />
         </View>
       </View>
