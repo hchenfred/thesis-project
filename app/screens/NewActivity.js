33e8177c5b9196c367b55b5786c57d0c81d6f0de
Huan Chen <hchen.fred@gmail.com>
2017-04-21 19:04:26 -0700

    (feat) create a new page to allow user to type in new activity for an event

--- app/screens/NewActivity.js
+++ app/screens/NewActivity.js
@@ -2,28 +2,67 @@ import React, { Component } from 'react';
 import ReactNative from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import ModalDropdown from 'react-native-modal-dropdown';
 import { Icon } from 'react-native-elements';
 import { ActionCreators } from '../actions';
 import {
   StyleSheet,
   View,
   Text,
+  Button,
+  TextInput,
   Image,
 } from 'react-native';
 
+const styles = StyleSheet.create({
+  textInput: {
+    margin: 20,
+    height: 40,
+  },
+});
+
+
 class NewActivity extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const { description, endTime, eventDate, id, location, name, photourl, startTime, username } = this.props.activeEvent;
     return (
       <View>
-        <Text>hello</Text>
+        <Text>{name}</Text>
+        <Text>{description}</Text>
+        <Text>{location}</Text>
+        <Text>host: {username}</Text>
+        <Text>start time: {startTime}</Text>
+        <Text>end time: {endTime}</Text>
+        <TextInput
+          style={styles.textInput}
+          clearTextOnFocus={true}
+          autoCorrect={false}
+          value={''}
+          placeholder="enter a place name"
+        />
+        <TextInput
+          style={styles.textInput}
+          clearTextOnFocus={true}
+          autoCorrect={false}
+          value={''}
+          placeholder="enter an address"
+        />
+        <Button
+          onPress={() => console.log('button is pressed')}
+          title="OK"
+          color="#841584"
+          accessibilityLabel="Ok, Great!"
+        />
       </View>
     );
   }
 }
 
 function mapStateToProps(state) {
-  return { simpleCounter: state.simpleCounter, user: state.user };
+  return { activeEvent: state.activeEvent };
 }
 
 function mapDispatchToProps(dispatch) {
