6dc3dcae2adbb57c3b1d6f6eaf7ce12c73820daf
ojolao <jolaosho@Omotayos-MacBook-Pro.local>
2017-04-22 16:01:23 -0700

    clarify instructions for using heroku server

--- app/config/global.js
+++ app/config/global.js
@@ -1,7 +1,9 @@
 let baseURL;
 
 // allows for multiuse url
-if (process.env.NODE_ENV === 'production') {
+// to switch to heroku server, change 'production' in line 5 to 'development';
+// to switch to local server, change 'development' in line 5 to 'production'
+if (process.env.NODE_ENV === 'development') {
   baseURL = 'https://hst-friend-ly.herokuapp.com';
 } else if (process.env.NODE_ENV === 'staging') {
   baseURL = 'https://hst-friend-ly-staging.herokuapp.com';
